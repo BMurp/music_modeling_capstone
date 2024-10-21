c2376f7185a79f469f5d31bcd9b0d8c881cd754e
Brendan Murphy <brendan@bmurphmedia.com>
2024-10-21 07:17:39 -0400

    cleanup config

--- configuration.py
+++ configuration.py
@@ -1,6 +1,7 @@
+#Defines project level configurations
+
 import os
 
-#Define project level configurations
 
 #make absolute file path of project available
 PROJECT_ABSOLUTE_PATH = os.path.abspath(os.path.dirname(__file__))
