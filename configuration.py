0c2bab862f9c92478138e91d2c49265e62f326bf
Brendan Murphy <brendan@bmurphmedia.com>
2024-11-30 16:06:55 -0500

    formatting on config , update on scenarios

--- configuration.py
+++ configuration.py
@@ -1,11 +1,8 @@
 #Defines project level configurations
-
 import os
-
 
 #make absolute file path of project available
 PROJECT_ABSOLUTE_PATH = os.path.abspath(os.path.dirname(__file__))
-
 
 #Folder For Data to use in relative references
 DATA_SOURCE_FOLDER = '/project_data_source/'
@@ -13,8 +10,7 @@ DATA_SOURCE_FOLDER = '/project_data_source/'
 #Full path to data within project's absolute path 
 DATA_SOURCE_PATH = PROJECT_ABSOLUTE_PATH + DATA_SOURCE_FOLDER
 
-
-###Expected location of fms metadata file csvs
+#Expected location of fms metadata file csvs
 FMA_METADATA_PATH = DATA_SOURCE_PATH +"free_music_archive/fma_metadata/"
 
 #relative path to the FMA audio
@@ -23,13 +19,12 @@ FMA_SMALL_AUDIO_PATH = DATA_SOURCE_FOLDER +"free_music_archive/fma_small/"
 FMA_MEDIUM_AUDIO_PATH = DATA_SOURCE_FOLDER +"free_music_archive/fma_medium/"
 FMA_LARGE_AUDIO_PATH = DATA_SOURCE_FOLDER +"free_music_archive/fma_large/"
 
-
-###Expected location of gtzan metadadata files 
+#Expected location of gtzan metadadata files 
 GTZAN_METADATA_PATH = DATA_SOURCE_PATH+"gtzan_dataset/Data/"
 
 #Gtzan relative path to audio files 
 GTZAN_AUDIO_PATH = DATA_SOURCE_FOLDER+"gtzan_dataset/Data/genres_original"
 
 #Model Input Data 
-MODEL_INPUT_DATA_FOLER = 'model_input_data/'
-MODEL_INPUT_DATA_PATH = DATA_SOURCE_PATH + MODEL_INPUT_DATA_FOLER
+MODEL_INPUT_DATA_FOLDER = 'model_input_data/'
+MODEL_INPUT_DATA_PATH = DATA_SOURCE_PATH + MODEL_INPUT_DATA_FOLDER
